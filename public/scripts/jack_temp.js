02dbc69cd5567cd9df99d858df3bd8f86519eb96
jathorp <jackthorp07@gmail.com>
2021-11-11 20:33:05 -0500

    added some links to jack_temp

--- public/scripts/jack_temp.js
+++ public/scripts/jack_temp.js
@@ -20,9 +20,13 @@
  * 
  * ######################################################################################################################################
  */
+
+// Fetch PDF as blodk from URL:
  const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
  const arrayBuffer = await fetch(url).then(res => res.arrayBuffer())
  const pdfDoc4 = await PDFDocument.load(arrayBuffer)
+
+// PDF Library: https://pdf-lib.js.org/docs/api/classes/pdfdocument#static-load
 
 
 /**
@@ -32,13 +36,10 @@
  * 
  * ######################################################################################################################################
  */
+
+// Canvas = HTML element -> Resolves canvas element to PNG blob
  let imageBlob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
 
-
 /**
- * ######################################################################################################################################
- * 
- * FILE UPLOAD/DOWNLOAD
- * 
- * ######################################################################################################################################
+ * For redrawing image onto canvas: https://www.w3schools.com/tags/canvas_drawimage.asp
  */
